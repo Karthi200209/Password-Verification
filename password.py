74edc60b8c02b0bb0d9a5d381d21b787921d3c30
Karthikeyan S <95049071+karthikeyanofficial@users.noreply.github.com>
2021-11-30 17:24:39 +0530

    Add files via upload

--- password.py
+++ password.py
@@ -1,3 +1,16 @@
+<<<<<<< HEAD
+import getpass
+database={"karthi":"2002",
+          "keyan":"2003"}
+username=input("Enter Your Username :")
+password=getpass.getpass("Enter Your Password:")
+for i in database.keys():
+    if username==1:
+        while password!=database.get(i):
+            password=getpass.getpass("Enter Password Again:")
+        break
+print("Verified")
+=======
 import getpass
 database={"karthi.keyan":"123456",
           "keyan.karthi":"654321"}
@@ -9,3 +22,4 @@ for i in database.keys():
             password=getpass.getpass("Enter Password Again:")
         break
 print("Verified")
+>>>>>>> af20b6da9f682efbacf020d7bb9f9c33324d38b2
